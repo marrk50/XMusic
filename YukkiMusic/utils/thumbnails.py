8d24e873a05737cb7e8dd58e73b604853f9e9876
marrk50 <105142154+marrk50@users.noreply.github.com>
2022-07-30 02:21:42 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -90,11 +90,11 @@ async def gen_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
+            (5, 5), f"MARK MUSIC", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
-            "NOW PLAYING",
+            "MARK PLAYER",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
